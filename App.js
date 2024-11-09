3ea7e9c1eb43c18b48724d90e77a619f71c2a600
SuhaniKapasiya <107610361+SuhaniKapasiya@users.noreply.github.com>
2024-11-09 10:45:13 +0530

    update coce

--- App.js
+++ App.js
@@ -1,54 +1,18 @@
-/**
- * <div id = "parent">
- *    <div id = "child">
- *        <h1>I am inside h1 tag</h1>
- *        <h1>I am inside h1 tag</h1>
- * 
- *    </div>
- *   <div id = "child">
- *        <h1>I am inside h1 tag</h1>
- *        <h1>I am inside h1 tag</h1>
- * 
- *    </div>
- * </div>
- * 
- * ReactElement(Object)=>HTML(Browser Understand)
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client"; // Make sure to use ReactDOM with proper capitalization
 
-const parent = React.createElement(
-  "div",
-  {id : "parent"},
- [
-   React.createElement(
-    "div",
-    {id :"child"},
-    [ 
-      React.createElement("h1",{},"I am inside h1 tag"),
-       React.createElement("h2",{},"I am inside h2 tag"),
-    ]
-  ),
-
-   React.createElement(
-    "div",
-    {id :"child"},
-    [ 
-      React.createElement("h1",{},"I am inside h1 tag"),
-       React.createElement("h2",{},"I am inside h2 tag"),
-    ]
-  
-
-  )
- ]
-);
-
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am inside Namaste React 🚀"),
+    React.createElement("h2", {}, "I am inside h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am inside h1 tag"),
+    React.createElement("h2", {}, "I am inside h2 tag"),
+  ]),
+]);
 
 console.log(parent);
 
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading", xyz: "abc" },
-//   "Hello world from React!"
-// );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# NamasteReact2024
- 
+# NamasteReact2024 🚀
+
+
+# Parecel
+-Dev Build 
+-Local Server
+-HMR = Hot Module Replacement
+-file watching Algorithm -written in C++
+-Caching - Faster Builds
+-Image  Optimization  
+-Menification
+-Bundling
+-Compress file 
+-consistent Hashing
+-code Splitting
+-Differential Bundling => support older browsers
+-Diagnostic - bueatiful error view to see
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code 
+-Different dev and prod bundles
